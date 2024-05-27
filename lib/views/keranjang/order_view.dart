c7ee5f95728998f61280c5de7c7e6ecf61964936
kinka1 <nufusmafaza@gmail.com>
2024-05-27 22:49:46 +0700

    feat: add keranjang

---
+++ lib/views/keranjang/order_view.dart
@@ -0,0 +1,358 @@
+import 'package:e_pos/data/model/order_detail/order_detail.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class OrderView extends StatefulWidget {
+  const OrderView({super.key});
+
+  @override
+  State<OrderView> createState() => _OrderViewState();
+}
+
+class _OrderViewState extends State<OrderView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Keranjang', style: TextStyle(color: Colors.white)),
+        leading: GestureDetector(
+          child: Icon(Icons.arrow_back, color: Colors.white),
+          onTap: () {
+            Navigator.pop(context);
+          },
+        ),
+        backgroundColor: Colors.blueAccent,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.delete_outline_rounded, color: Colors.white),
+            onPressed: () {
+              _setAlertDelete(context);
+            },
+          ),
+        ],
+      ),
+      backgroundColor: Colors.grey[100],
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+              child: Column(children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Icon(Icons.bookmark, color: Colors.blue[600]),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 12),
+                      child: Text('Rincian Transaksi'),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "ID Transaksi",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text("123456")
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 12),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Tanggal Transaksi",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text("23/04/1999")
+                    ],
+                  ),
+                ),
+              ]),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return _buildCard("Sabun", 20, 8000);
+              },
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                  color: Colors.grey[300]!,
+                  width: 1,
+                ),
+              ),
+              color: Colors.white,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Bayar",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text("Rp. 100.000"),
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                  color: Colors.grey[300]!,
+                  width: 1,
+                ),
+              ),
+              color: Colors.white,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Kembalian",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text("Rp. 0"),
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 24, vertical: 39),
+            color: Colors.blue[600],
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Bayar (Check Out)",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.white),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "Rp. 100.000",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.white),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        _setAlert(context);
+                      },
+                      icon: Icon(Icons.arrow_forward),
+                      color: Colors.white,
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCard(String title, int qty, int price) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(24, 0, 24, 12),
+      child: Container(
+        width: double.infinity,
+        padding: EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white,
+          border: Border.all(
+            color: Colors.white,
+            width: 1,
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
+                Text(
+                  "${qty} x ${price}",
+                  style: TextStyle(
+                      color: Colors.grey[400], fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Rp${(qty * price).toString()}",
+                  style: TextStyle(color: Colors.blue[600]),
+                ),
+              ],
+            ),
+            // CustomNumberInputWithIncrementDecrement(value: 3, )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<bool> _setAlert(BuildContext context) async {
+    return await showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(6),
+        ),
+        title: Center(
+          child: Text(
+            'Transaksi Berhasil',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+              color: Colors.green[600],
+            ),
+          ),
+        ),
+        content: Icon(
+          Icons.check_box_rounded,
+          color: Colors.green[600],
+          size: 32,
+        ),
+        actions: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  margin: EdgeInsets.symmetric(vertical: 5),
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.blue),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Detail Transaksi',
+                      style: TextStyle(color: Colors.blue[600]),
+                    ),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).pop(false);
+                },
+                child: Container(
+                  margin: EdgeInsets.symmetric(vertical: 5),
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Kembali',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<bool> _setAlertDelete(BuildContext context) async {
+    return await showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(6),
+        ),
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        title: Center(
+          child: Text(
+            'Hapus Keranjang?',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+              color: Colors.red[600],
+            ),
+          ),
+        ),
+        content: Text("Aksi ini akan membatalkan transaksi",
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.grey[600])),
+        actions: [
+          Row(
+
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).pop(false);
+                  },
+                  child: Container(
+                    margin: EdgeInsets.all(1),
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      // color: const Color.fromARGB(255, 255, 255, 255),
+                      border: Border.all(color: Colors.grey[600]!),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Tidak',
+                        style: TextStyle(color: Colors.grey[600]!),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(width: 12,),
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                  },
+                  child: Container(
+                    margin: EdgeInsets.symmetric(vertical: 5),
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: Colors.red[500],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Hapus',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
