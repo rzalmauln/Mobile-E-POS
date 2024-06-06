fe0138ce0967ba56e0135f0f497758875e291adf
kinka1 <nufusmafaza@gmail.com>
2024-06-06 12:07:13 +0700

    fix: conflict

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: OrderView(),
+          child: StoreScreen(),
         ),
       ),
     );
